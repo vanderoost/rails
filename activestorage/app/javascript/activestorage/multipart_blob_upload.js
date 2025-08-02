06a9326b8c26ef70104b4801b8eecf7164d298c0
Richard van der Oost <richard@vanderoost.com>
2025-08-02 16:55:47 +0100

    WIP: Concurrent uploads

--- activestorage/app/javascript/activestorage/multipart_blob_upload.js
+++ activestorage/app/javascript/activestorage/multipart_blob_upload.js
@@ -9,49 +9,80 @@ export class MultipartBlobUpload {
     this.partSize = part_size
     this.partUrls = part_urls
     this.uploadedParts = []
-    this.currentPartIndex = 0
-
-    console.debug("blobId:", this.blobId)
-    console.debug("uploadId:", this.uploadId)
+    this.maxConcurrentUploads = 3
 
     // Add a dummy xhr for compatibility with the notify system
     this.xhr = new XMLHttpRequest()
   }
 
   create(callback) {
-    console.debug("MultipartBlobUpload#create starting multipart upload")
     this.callback = callback
-    this.uploadNextPart()
+    this.uploadPartsConcurrently()
   }
 
-  uploadNextPart() {
-    if (this.currentPartIndex >= this.partUrls.length) {
-      // All parts uploaded, complete the multipart upload
-      console.debug("All parts uploaded, completing multipart upload")
-      this.completeMultipartUpload()
-      return
-    }
+  uploadPartsConcurrently() {
+    this.uploadPartsWithConcurrencyLimit(this.partUrls, this.maxConcurrentUploads)
+      .then(() => {
+        console.debug("All parts uploaded, completing multipart upload")
+        this.completeMultipartUpload()
+      })
+      .catch(error => { this.callback(error) })
+  }
 
-    const partData = this.partUrls[this.currentPartIndex]
-    const start = (partData.part_number - 1) * this.partSize
-    const end = Math.min(start + this.partSize, this.file.size)
-    const chunk = this.file.slice(start, end)
+  uploadPartsWithConcurrencyLimit(parts, limit) {
+    return new Promise((resolve, reject) => {
+      const results = []
+      const executing = []
+      let partIndex = 0
 
-    console.debug(`Uploading part ${partData.part_number}/${this.partUrls.length}`)
+      const startNextUpload = () => {
+        if (partIndex >= parts.length) {
+          if (executing.length === 0) {
+            resolve()
+          }
+          return
+        }
 
-    this.uploadPart(partData.url, chunk, (error, etag) => {
-      if (error) {
-        this.callback(error)
-        return
+        const partData = parts[partIndex++]
+        const uploadPromise = this.uploadPartAsync(partData)
+
+        results.push(uploadPromise)
+        executing.push(uploadPromise)
+
+        uploadPromise
+          .then(() => {
+            executing.splice(executing.indexOf(uploadPromise), 1)
+            startNextUpload()
+          })
+          .catch(error => {
+            executing.splice(executing.indexOf(uploadPromise), 1)
+            reject(error)
+          })
       }
 
-      this.uploadedParts.push({
-        etag: etag,
-        part_number: partData.part_number,
-      })
+      for (let i = 0; i < Math.min(limit, parts.length); i++) {
+        startNextUpload()
+      }
+    })
+  }
 
-      this.currentPartIndex++
-      this.uploadNextPart()
+  uploadPartAsync(partData) {
+    return new Promise((resolve, reject) => {
+      const start = (partData.part_number - 1) * this.partSize
+      const end = Math.min(start + this.partSize, this.file.size)
+      const chunk = this.file.slice(start, end)
+
+      console.debug(`Part ${partData.part_number}/${this.partUrls.length} starting`)
+
+      this.uploadPart(partData.url, chunk, (error, etag) => {
+        if (error) {
+          reject(error)
+        } else {
+          this.uploadedParts.push({ etag: etag, part_number: partData.part_number })
+          console.debug(`Part ${partData.part_number}/${this.partUrls.length} uploaded`)
+          resolve(etag)
+        }
+      })
     })
   }
 
@@ -75,6 +106,9 @@ export class MultipartBlobUpload {
   }
 
   completeMultipartUpload() {
+    // Sort parts by part_number to ensure correct order
+    this.uploadedParts.sort((a, b) => a.part_number - b.part_number)
+
     const xhr = new XMLHttpRequest()
     const completeUrl = `/rails/active_storage/direct_uploads/${this.blobId}`
 
