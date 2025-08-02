19e79ce381d991202d8c7ae9f4a512fa0b2bb440
Richard van der Oost <richard@vanderoost.com>
2025-08-02 16:55:47 +0100

    WIP: Concurrent uploads

--- activestorage/app/javascript/activestorage/direct_uploads_controller.js
+++ activestorage/app/javascript/activestorage/direct_uploads_controller.js
@@ -7,30 +7,85 @@ export class DirectUploadsController {
   constructor(form) {
     this.form = form
     this.inputs = findElements(form, inputSelector).filter(input => input.files.length)
+    this.maxConcurrentUploads = 3 // Upload up to 3 files concurrently
   }
 
   start(callback) {
     const controllers = this.createDirectUploadControllers()
+    this.dispatch("start")
 
-    const startNextController = () => {
-      const controller = controllers.shift()
-      if (controller) {
-        controller.start(error => {
-          if (error) {
-            callback(error)
-            this.dispatch("end")
-          } else {
-            startNextController()
-          }
-        })
-      } else {
-        callback()
-        this.dispatch("end")
-      }
+    if (controllers.length === 0) {
+      callback()
+      this.dispatch("end")
+      return
     }
 
-    this.dispatch("start")
-    startNextController()
+    // Upload files concurrently
+    this.uploadControllersConcurrently(controllers, callback)
+  }
+
+  uploadControllersConcurrently(controllers, callback) {
+    console.debug("DirectUploadsController#startNextController")
+    
+    this.uploadControllersWithConcurrencyLimit(controllers, this.maxConcurrentUploads)
+      .then(() => {
+        callback()
+        this.dispatch("end")
+      })
+      .catch(error => {
+        callback(error)
+        this.dispatch("end")
+      })
+  }
+
+  uploadControllersWithConcurrencyLimit(controllers, limit) {
+    return new Promise((resolve, reject) => {
+      const results = []
+      const executing = []
+      let controllerIndex = 0
+
+      const startNextUpload = () => {
+        if (controllerIndex >= controllers.length) {
+          if (executing.length === 0) {
+            resolve()
+          }
+          return
+        }
+
+        const controller = controllers[controllerIndex++]
+        const uploadPromise = this.uploadControllerAsync(controller)
+        
+        results.push(uploadPromise)
+        executing.push(uploadPromise)
+
+        uploadPromise
+          .then(() => {
+            executing.splice(executing.indexOf(uploadPromise), 1)
+            startNextUpload()
+          })
+          .catch(error => {
+            executing.splice(executing.indexOf(uploadPromise), 1)
+            reject(error)
+          })
+      }
+
+      // Start up to 'limit' concurrent uploads
+      for (let i = 0; i < Math.min(limit, controllers.length); i++) {
+        startNextUpload()
+      }
+    })
+  }
+
+  uploadControllerAsync(controller) {
+    return new Promise((resolve, reject) => {
+      controller.start(error => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve()
+        }
+      })
+    })
   }
 
   createDirectUploadControllers() {
